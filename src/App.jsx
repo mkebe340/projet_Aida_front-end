98534d814c462a62ed3f0c0ed53311f005cd59cd
zina <keiraayari@gmail.com>
2020-12-10 10:59:34 +0100

    effacement des photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 //import './App.css';
 import Navbar from './components/Pages/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 
--- src/components/Pages/QuiSommesNous.jsx
+++ src/components/Pages/QuiSommesNous.jsx
@@ -1,28 +1,86 @@
 import React from 'react';
+import '../style/QuiSommesNous.css';
 
 class QuiSommesNous extends React.Component {
 
     render() {
         return (
 
-           
-                <div> 
-                    <h3> Nos Inspires </h3>
 
-                    <div class="container1">
-                        <div class="row">
-                            <div class="color3 carre  col order-last">
-                                <div className='titre'>
-                                    <h3> <a href="#" title='titre'>Titre </a> </h3>
+            <div>
+                <h3> Nos Inspires </h3>
+                <div className="container">
+                    <div className="slide1 col lg-4 sm-4 ">
+                        {/* <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                            <ol className="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1" className="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2" className="active"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="../img/Portrait.jpg" class="d-block w-50" alt="portrait" />
                                 </div>
-                                <div className='text'>
-
-                                    <p><a href="#" text='text'> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                        qui officia deserunt mollit anim id est laborum."</a></p>
+                                <div className="carousel-item">
+                                    <img src="../img/Report_3.jpg" class="d-block w-50" alt="photo reportage" />
                                 </div>
-
+                                <div className="carousel-item">
+                                    <img src="../img/Report_1.jpg" class="d-block w-50" alt="photo reportage" />
+                                </div>
                             </div>
+                            <a href="#carouselExampleIndicators" className="carousel-control-prev" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a href="#carouselExampleIndicators" className="carousel-control-next" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div> */}
 
+
+                        {/* Exemple Bootstrap */}
+                        {/* <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                            <ol className="carousel-indicators">
+                                <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="../img/Portrait.jpg" className="d-block w-100" alt="..." />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>First slide label</h5>
+                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="../img/Report_3.jpg" className="d-block w-100" alt="..." />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Second slide label</h5>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </div>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="../img/Report_1.jpg" className="d-block w-100" alt="..." />
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Third slide label</h5>
+                                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
         )
     }
 };

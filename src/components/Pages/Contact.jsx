588fbe889a19e94c820e0a46d575f2d76e374f5f
mkebe340 <68587998+mkebe340@users.noreply.github.com>
2020-12-10 21:29:47 +0100

    home + contact mise en forme

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../style/Contact.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Contact extends React.Component {
 
@@ -7,7 +9,32 @@ class Contact extends React.Component {
 
             <div>
                 <h1>Contact </h1>
+                <div className="container test ">
+                    <div className='row justify-content-center'>
+                        <div className="col-6 color mt-5">
 
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Earum temporibus dolorum maxime voluptates magni vel
+                            reprehenderit rem? Asperiores nam nostrum earum eos,
+                            incidunt quo, ducimus neque, nemo optio cumque
+                             ratione.</p>
+                        </div>
+
+
+
+                        <div className="row">
+                            <div className="col-6 " >
+
+                                <form>
+                                    <input className='text' ></input>
+                                    <input className='text' ></input>
+                                    <input className='text' ></input>
+                                </form>
+                                <button>envoyer</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         )
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,13 +1,51 @@
 
 import React from 'react';
+import '../style/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import APropos from '../Pages/APropos';
 
 class Home extends React.Component {
 
     render() {
         return (
 
-            <div>
-                <h1> Home </h1>
+            <div >
+                
+                <div className='container' >
+                
+                    <div className='row justify-content-around'>
+                    
+                        <div className='col-6 mt-5 align-self-center'>
+                        <h1> Titre Home </h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis cumque magnam nulla aut recusandae veritatis reprehenderit magni exercitationem harum officia molestiae numquam
+                             odit, quis fugit delectus? Voluptatem qui rem aliquid.</p>
+                            <Link to='/APropos'>
+                                <button> A props</button>
+                            </Link>
+                        </div>
+
+                        <div className=" col-4 mt-5">
+                            <div className='boite1'>
+                                <h1>BOITE 1</h1>
+                            </div>
+
+
+                            <div className='boite2  mt-5  '>
+                                <h1>BOITE 2</h1>
+                            </div>
+
+                            <div className='boite2  mt-5 '>
+                                <h1>BOITE 3</h1>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+
 
             </div>
 
